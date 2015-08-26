bce3eeba32a042f7e3a5c0ba7acb3ec20ea65e23
Milan Otahal <otahal@itstudio.cz>
2015-08-26 14:43:00 +0200

    refactoring SubscriberGroup

--- src/CentralNews/Entity/SubscriberGroup.php
+++ src/CentralNews/Entity/SubscriberGroup.php
@@ -2,14 +2,8 @@
 
 namespace CentralNews\Entity;
 
-use CentralNews\Exception\DomainException;
-use CentralNews\Exception\InvalidArgumentException;
-
-class SubscriberGroup extends Entity
+class SubscriberGroup extends BaseSubscriberGroup
 {
-    /** @var string */
-    protected $name;
-
     /** @var string */
     protected $description;
 
@@ -20,25 +14,9 @@ class SubscriberGroup extends Entity
     {
         parent::__construct($data);
 
-        if (isset($data['name'])) {
-            $this->setName($data['name']);
-        }
-
         if (isset($data['description'])) {
             $this->setDescription($data['description']);
         }
-    }
-
-    /**
-     * @throws CentralNews\Exception\DomainException   
-     * @return string
-     */
-    public function getName()
-    {
-        if (!$this->name) {
-            throw new DomainException;
-        }
-        return $this->name;
     }
 
     /**
@@ -47,22 +25,6 @@ class SubscriberGroup extends Entity
     public function getDescription()
     {
         return $this->description;
-    }
-
-    /**
-     * 
-     * @param string $name
-     * @throws \CentralNews\Exception\InvalidArgumentException
-     * @return this
-     */
-    public function setName($name)
-    {
-        $name = (string) $name;
-        if (empty($name)) {
-            throw new InvalidArgumentException;
-        }
-        $this->name = $name;
-        return $this;
     }
 
     /**
